16afb28a6df5bbdf2461977ad35898c1713f1b64
rogojagad <rogojagadalit@gmail.com>
2018-05-18 11:51:23 +0700

    betulin route ngaco gara2 lucas

--- routes/web.php
+++ routes/web.php
@@ -11,79 +11,74 @@
 |
 */
 
-Auth::routes();
-
-Route::get('/', 	'GuestController@home');
-Route::get('/term', 'GuestController@term');
-
+Route::get('/', function () {
+    return view('home');
+});
 
 Route::get('/bayar','KegiatanController@show');
 
-Route::get('/galang', function () {
-	if(!Auth::check()){
-		return redirect('/register');
-	}
-	else if(Auth::user()->tipe_user == 0){
-		return redirect('/user/'.Auth::user()->id.'/verifikasi');
-	}
-	else{
-    	return view('galang');
-	}
+Route::get('/user', function () {
+    return view('user');
+});
+Route::get('/termgalang', function () {
+    return view('termgalang');
 });
 
-Route::get('/users', 'UserController@index');
-
-Route::post('/galang/{u}/upload'	, 'KegiatanController@store');
-Route::get('/transaksi/{u}/delete'	, 'TransaksiController@destroy');
-
+Route::get('/galang', function () {
+    if(!Auth::check()){
+        return redirect('/register');
+    }
+    else if(Auth::user()->tipe_user == 0){
+        return redirect('/user/'.Auth::user()->id.'/verifikasi');
+    }
+    else{
+        return view('galang');
+    }
+});
 Route::get('/verifikasi', function () {
     return view('verifikasiakun');
 });
+Route::get('/users', 'UserController@index');
+Route::get('/user/{u}/profil', 'UserController@edit');
+Route::post('/user/{u}/update', 'UserController@update');
+Route::post('/user/{u}/image','UserController@imageUpload');
+Route::get('/user/{u}/transaksi', 'UserController@transaksi');
+Route::get('/user/{u}/achievement', 'UserController@achievement');
+Route::get('/user/{u}/verifikasi', function(){
+    return view('verifikasiakun');
+});
+Route::post('/user/{u}/verifikasi', 'UserController@verifikasi');
+Route::get('/user/{u}/campaign','UserController@campaign');
+
+Route::get('/donasi/{u}/detail', 'KegiatanController@show');
+Route::get('/donasi/{u}/transaksi', 'KegiatanController@show2');
+Route::post('/donasi/{u}/bayar', 'TransaksiController@store');
+Route::get('/donasi', 'KegiatanController@index');
+Route::post('/galang/{u}/upload', 'KegiatanController@store');
+Route::get('/donasi/{u}/cair', 'KegiatanController@verifikasicair');
+Route::get('/donasi/{u}/delete', 'KegiatanController@destroy');
+Route::get('/transaksi/{u}/delete', 'TransaksiController@destroy');
 
 
-Route::get('cariuser', 		'AdminController@search');
-Route::get('caricampaign', 	'AdminController@searchCampaign');
+Route::get('/campaign/{u}/updateForm', 'KegiatanController@updateForm');
+Route::post('/campaign/{u}/update', 'KegiatanController@update');
 
 
+Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/logout', function(){
-	Auth::logout();
-	return redirect('/');
+    Auth::logout();
+    return redirect('/');
 });
 
-Route::prefix('admin')->group(function() {
-	Route::get ('/'							, 'AdminController@index');
-	Route::get ('campaign'					, 'AdminController@indexCampaign');
-	Route::get ('campaign/{u}/verifikasi'	, 'AdminController@showCampaign');
-	Route::get ('donasi'					, 'AdminController@indexDonasi');
-	Route::get ('donasi/{u}/verifikasi'		, 'AdminController@showDonasi');
-	Route::get ('{u}/verifikasi'			, 'AdminController@show');
-
-	Route::post('{u}/verifikasi'			, 'AdminController@verifikasi');
-	Route::post('campaign/{u}/verifikasi'	, 'AdminController@verifikasiCampaign');
-	Route::post('donasi/{u}/verifikasi'		, 'AdminController@verifikasiDonasi');
-});
-
-Route::prefix('campaign')->group(function() {
-	Route::get ('{u}/updateForm'	, 'KegiatanController@updateForm');
-	Route::get ('{u}/achievement'	, 'UserController@achievement');
-	Route::get ('{u}/campaign'		, 'UserController@campaign');
-	Route::get ('{u}/profil'		, 'UserController@edit');
-	Route::get ('{u}/transaksi'		, 'UserController@transaksi');
-	Route::get ('{u}/verifikasi'	, 'UserController@view_verifikasi');
-
-	Route::post('{u}/image'			, 'UserController@imageUpload');
-	Route::post('{u}/verifikasi'	, 'UserController@verifikasi');
-	Route::post('{u}/update'		, 'UserController@update');
-	// Route::post('{u}/update'		, 'KegiatanController@update');
-});
-
-Route::prefix('donasi')->group(function() {
-	Route::get ('/'				, 'KegiatanController@index');
-	Route::get ('{u}/cair'		, 'KegiatanController@verifikasicair');
-	Route::get ('{u}/delete'	, 'KegiatanController@destroy');
-	Route::get ('{u}/detail'	, 'KegiatanController@show');
-	Route::get ('{u}/transaksi'	, 'KegiatanController@show2');
-
-	Route::post('{u}/bayar'		, 'TransaksiController@store');
-});
+Route::get('/admin', 'AdminController@index');
+Route::get('/admin/campaign', 'AdminController@indexCampaign');
+Route::get('/admin/donasi', 'AdminController@indexDonasi');
+Route::get('/admin/{u}/verifikasi', 'AdminController@show');
+Route::get('/admin/campaign/{u}/verifikasi', 'AdminController@showCampaign');
+Route::get('/admin/donasi/{u}/verifikasi', 'AdminController@showDonasi');
+Route::post('/admin/{u}/verifikasi', 'AdminController@verifikasi');
+Route::post('/admin/campaign/{u}/verifikasi', 'AdminController@verifikasiCampaign');
+Route::post('/admin/donasi/{u}/verifikasi', 'AdminController@verifikasiDonasi');
+Route::get('cariuser',"AdminController@search");
+Route::get('caricampaign',"AdminController@searchCampaign");
